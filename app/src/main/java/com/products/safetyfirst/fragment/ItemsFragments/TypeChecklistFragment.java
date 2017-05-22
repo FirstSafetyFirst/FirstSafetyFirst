d25dd417868a17913400fa40228bb98b976a8041
Rishabh Gupta <rishabh4325@gmail.com>
2017-05-22 12:35:12 +0530

    Checklist to know it type added

--- app/src/main/java/com/products/safetyfirst/fragment/ItemsFragments/TypeChecklistFragment.java
+++ app/src/main/java/com/products/safetyfirst/fragment/ItemsFragments/TypeChecklistFragment.java
@@ -1,6 +1,9 @@
 package com.products.safetyfirst.fragment.ItemsFragments;
 
 
+import android.content.Intent;
+import android.content.res.TypedArray;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -8,6 +11,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.products.safetyfirst.R;
+import com.products.safetyfirst.activity.ItemTypeInfoActivity;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -27,4 +31,21 @@ public class TypeChecklistFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_type_checklist, container, false);
     }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        String url;
+
+        int toolValue = getArguments().getInt(ItemTypeInfoActivity.tool, 0);
+        int typeValue = getArguments().getInt(ItemTypeInfoActivity.typeNumber, 0);
+
+        TypedArray ta = getResources().obtainTypedArray(R.array.checklist_embed);
+        url = getResources().getStringArray(ta.getResourceId(toolValue, 0))[typeValue];
+//        Intent intent = new Intent(Intent.ACTION_VIEW);
+//        intent.setDataAndType(Uri.parse(url), "application/pdf");
+//        intent.setFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+        startActivity(intent);
+    }
 }
+// TODO : ANDROID PERMISSION WRITE_eXTERNAL_STORAGE
