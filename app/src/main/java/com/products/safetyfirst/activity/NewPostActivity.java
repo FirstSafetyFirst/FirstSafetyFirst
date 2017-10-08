f5fb408254a44ff182a49d927066714f09dfc92d
Rishabh Gupta(zeerorg) <rishabh4325@gmail.com>
2017-10-08 10:53:00 +0530

    Added images taking

--- app/src/main/java/com/products/safetyfirst/activity/NewPostActivity.java
+++ app/src/main/java/com/products/safetyfirst/activity/NewPostActivity.java
@@ -1,20 +1,37 @@
 package com.products.safetyfirst.activity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 import com.products.safetyfirst.R;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 import jp.wasabeef.richeditor.RichEditor;
 
 public class NewPostActivity extends AppCompatActivity implements View.OnClickListener {
 
+    /* Views */
     private RichEditor editor;
     private ImageButton boldBtn;
     private ImageButton italicBtn;
     private ImageButton underlineBtn;
+    private ImageButton pickImgBtn;
+    private Button createPostBtn;
+
+    /* Constants */
+    private static final int PICK_IMAGE = 233;
+
+    /* Variables used */
+    private List<Bitmap> images = new ArrayList<>();
 
 
     @Override
@@ -26,18 +43,22 @@ public class NewPostActivity extends AppCompatActivity implements View.OnClickLi
         boldBtn = (ImageButton) findViewById(R.id.bold_btn);
         italicBtn = (ImageButton) findViewById(R.id.italic_btn);
         underlineBtn = (ImageButton) findViewById(R.id.underline_btn);
+        pickImgBtn = (ImageButton) findViewById(R.id.pick_img_btn);
+        createPostBtn = (Button) findViewById(R.id.post_btn);
 
         initEditor();
     }
 
     void initEditor() {
-        editor.setEditorHeight(200);
+        editor.setEditorHeight(400);
         editor.setPadding(10, 10, 50, 10);
         editor.setPlaceholder("Type question...");
 
         boldBtn.setOnClickListener(this);
         italicBtn.setOnClickListener(this);
         underlineBtn.setOnClickListener(this);
+        pickImgBtn.setOnClickListener(this);
+        createPostBtn.setOnClickListener(this);
     }
 
     @Override
@@ -48,6 +69,31 @@ public class NewPostActivity extends AppCompatActivity implements View.OnClickLi
             case R.id.bold_btn: editor.setBold(); break;
             case R.id.italic_btn: editor.setItalic(); break;
             case R.id.underline_btn: editor.setUnderline(); break;
+            case R.id.pick_img_btn: pickImage(); break;
+            case R.id.post_btn: createPost(); break;
+        }
+    }
+
+    void pickImage(){
+        Intent getIntent = new Intent(Intent.ACTION_GET_CONTENT);
+        getIntent.setType("image/*");
+
+        Intent pickIntent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        pickIntent.setType("image/*");
+
+        Intent chooserIntent = Intent.createChooser(getIntent, "Select Image");
+        chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, new Intent[] {pickIntent});
+
+        startActivityForResult(chooserIntent, PICK_IMAGE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == PICK_IMAGE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            images.add(imageBitmap);
         }
     }
 }
