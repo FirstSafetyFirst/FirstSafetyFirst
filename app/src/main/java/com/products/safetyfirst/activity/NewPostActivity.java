1f15b926a3786c2e210070587b9bc42d6abae140
Rishabh Gupta(zeerorg) <rishabh4325@gmail.com>
2017-10-08 18:17:19 +0530

    Added images add button

--- app/src/main/java/com/products/safetyfirst/activity/NewPostActivity.java
+++ app/src/main/java/com/products/safetyfirst/activity/NewPostActivity.java
@@ -1,9 +1,13 @@
 package com.products.safetyfirst.activity;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -29,6 +33,7 @@ public class NewPostActivity extends AppCompatActivity implements View.OnClickLi
 
     /* Constants */
     private static final int PICK_IMAGE = 233;
+    private static final int REQUEST_READ = 12;
 
     /* Variables used */
     private List<Bitmap> images = new ArrayList<>();
@@ -70,26 +75,29 @@ public class NewPostActivity extends AppCompatActivity implements View.OnClickLi
             case R.id.italic_btn: editor.setItalic(); break;
             case R.id.underline_btn: editor.setUnderline(); break;
             case R.id.pick_img_btn: pickImage(); break;
-            case R.id.post_btn: createPost(); break;
+            //case R.id.post_btn: createPost(); break;
         }
     }
 
     void pickImage(){
-        Intent getIntent = new Intent(Intent.ACTION_GET_CONTENT);
-        getIntent.setType("image/*");
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
 
-        Intent pickIntent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-        pickIntent.setType("image/*");
+                Intent getIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                getIntent.setType("image/*");
 
-        Intent chooserIntent = Intent.createChooser(getIntent, "Select Image");
-        chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, new Intent[] {pickIntent});
+                startActivityForResult(getIntent, PICK_IMAGE);
 
-        startActivityForResult(chooserIntent, PICK_IMAGE);
+            } else {
+                requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, REQUEST_READ);
+            }
+        }
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
+        //super.onActivityResult(requestCode, resultCode, data);
+        Log.e("Image", "Received");
         if(requestCode == PICK_IMAGE && resultCode == RESULT_OK) {
             Bundle extras = data.getExtras();
             Bitmap imageBitmap = (Bitmap) extras.get("data");
