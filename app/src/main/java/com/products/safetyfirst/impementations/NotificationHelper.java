cb6405f05787450351f7225f0601fc8533257c71
Rishabh Gupta(zeerorg) <rishabh4325@gmail.com>
2017-10-16 09:32:53 +0530

    Added notification after posting

---
+++ app/src/main/java/com/products/safetyfirst/impementations/NotificationHelper.java
@@ -0,0 +1,49 @@
+package com.products.safetyfirst.impementations;
+
+import android.app.NotificationManager;
+import android.content.Context;
+import android.support.v4.app.NotificationCompat;
+
+import com.products.safetyfirst.R;
+import com.products.safetyfirst.interfaces.view.SimpleNotification;
+
+import java.util.Random;
+
+/**
+ * Created by rishabh on 15/10/17.
+ */
+
+public class NotificationHelper implements SimpleNotification {
+    private static final NotificationHelper ourInstance = new NotificationHelper();
+
+    public static NotificationHelper getInstance() {
+        return ourInstance;
+    }
+
+    private NotificationHelper() {
+    }
+
+    /**
+     * Creates a simple Notification without any intent
+     * @param context
+     * @param title
+     * @param text
+     * @return Notification Id which can later be used
+     */
+    @Override
+    public int createNotif(Context context, String title, String text) {
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context)
+                        .setSmallIcon(R.mipmap.ic_launcher)
+                        .setContentTitle(title)
+                        .setContentText(text);
+        NotificationManager mNotificationManager =
+                (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        Random random = new Random();
+        int mNotificationId = random.nextInt(1000) + 100;
+
+        mNotificationManager.notify(mNotificationId, mBuilder.build());
+
+        return mNotificationId;
+    }
+}
